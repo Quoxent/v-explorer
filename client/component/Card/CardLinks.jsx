d4b65cf161f3756439cc31f31201b045ac5b0454
dustinengle <dustin.engle@gmail.com>
2018-08-05 11:07:15 -0700

    rebrand name, icons, and images

--- client/component/Card/CardLinks.jsx
+++ client/component/Card/CardLinks.jsx
@@ -5,15 +5,15 @@ import Card from './Card';
 
 const CardLinks = () => (
   <Card title="Links">
-    <a href="https://bulwarkcrypto.com/" target="_blank">Website</a><br />
+    <a href="https://vulcanocrypto.com/" target="_blank">Website</a><br />
     <a href="https://bitcointalk.org/index.php?topic=2499481.0" target="_blank">Bitcointalk</a><br />
-    <a href="https://github.com/bulwark-crypto" target="_blank">Github</a><br />
-    <a href="https://www.reddit.com/r/BulwarkCoin/" target="_blank">Reddit</a><br />
+    <a href="https://github.com/vulcano-crypto" target="_blank">Github</a><br />
+    <a href="https://www.reddit.com/r/VulcanoCoin/" target="_blank">Reddit</a><br />
     <a href="https://discord.gg/a7vhegP" target="_blank">Discord</a><br />
-    <a href="https://t.me/bulwarkcrypto" target="_blank">Telegram</a><br />
-    <a href="https://twitter.com/BulwarkCoin" target="_blank">Twitter</a><br />
-    <a href="https://www.facebook.com/bulwark.coin.IO/" target="_blank">Facebook</a>
-    <a href="https://medium.com/@bulwarktube" target="_blank">Medium</a>
+    <a href="https://t.me/vulcanocrypto" target="_blank">Telegram</a><br />
+    <a href="https://twitter.com/VulcanoCoin" target="_blank">Twitter</a><br />
+    <a href="https://www.facebook.com/vulcano.coin.IO/" target="_blank">Facebook</a>
+    <a href="https://medium.com/@vulcanotube" target="_blank">Medium</a>
   </Card>
 );
 
--- script/bulwarkd_setup.sh
+++ script/bulwarkd_setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # Download latest node and install.
-bwklink=`curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep browser_download_url | grep linux64 | cut -d '"' -f 4`
-mkdir -p /tmp/bulwark
-cd /tmp/bulwark
-curl -Lo bulwark.tar.gz $bwklink
-tar -xzf bulwark.tar.gz
+bwklink=`curl -s https://api.github.com/repos/vulcano-crypto/vulcano/releases/latest | grep browser_download_url | grep linux64 | cut -d '"' -f 4`
+mkdir -p /tmp/vulcano
+cd /tmp/vulcano
+curl -Lo vulcano.tar.gz $bwklink
+tar -xzf vulcano.tar.gz
 sudo mv ./bin/* /usr/local/bin
 cd
-rm -rf /tmp/bulwark
-mkdir ~/.bulwark
+rm -rf /tmp/vulcano
+mkdir ~/.vulcano
 
 # Setup configuration for node.
 rpcuser=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
 rpcpassword=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
-cat >~/.bulwark/bulwark.conf <<EOL
+cat >~/.vulcano/vulcano.conf <<EOL
 rpcuser=$rpcuser
 rpcpassword=$rpcpassword
 daemon=1
@@ -21,4 +21,4 @@ txindex=1
 EOL
 
 # Start node.
-bulwarkd
+vulcanod
