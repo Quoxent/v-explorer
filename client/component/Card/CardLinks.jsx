b9d26991d888a5c5c540734d68f9c9be1104a25b
dustinengle <dustin.engle@gmail.com>
2018-08-09 17:58:31 -0700

    updated coin info page

--- client/component/Card/CardLinks.jsx
+++ client/component/Card/CardLinks.jsx
@@ -6,14 +6,11 @@ import Card from './Card';
 const CardLinks = () => (
   <Card title="Links">
     <a href="https://vulcano.io/" target="_blank">Website</a><br />
-    <a href="https://bitcointalk.org/index.php?topic=2499481.0" target="_blank">Bitcointalk</a><br />
-    <a href="https://github.com/vulcano-crypto" target="_blank">Github</a><br />
-    <a href="https://www.reddit.com/r/VulcanoCoin/" target="_blank">Reddit</a><br />
-    <a href="https://discord.gg/a7vhegP" target="_blank">Discord</a><br />
-    <a href="https://t.me/vulcanocrypto" target="_blank">Telegram</a><br />
-    <a href="https://twitter.com/VulcanoCoin" target="_blank">Twitter</a><br />
-    <a href="https://www.facebook.com/vulcano.coin.IO/" target="_blank">Facebook</a>
-    <a href="https://medium.com/@vulcanotube" target="_blank">Medium</a>
+    <a href="https://bitcointalk.org/index.php?topic=2202326.0" target="_blank">BitcoinTalk</a><br />
+    <a href="https://bitcoingarden.org/forum/index.php?topic=18163.0" traget="_blank">BitcoinGarden</a><br />
+    <a href="https://github.com/VulcanoCrypto" target="_blank">Github</a><br />
+    <a href="https://discord.gg./YTHKAS3" target="_blank">Discord</a><br />
+    <a href="https://twitter.com/vulcanocoin" target="_blank">Twitter</a><br />
   </Card>
 );
 
