7d98978ef980b0e3131a5722abd6657301b3bdd3
dustinengle <dustin.engle@gmail.com>
2018-08-28 13:23:42 -0700

    open link in new tab fix

--- client/component/Card/CardLinks.jsx
+++ client/component/Card/CardLinks.jsx
@@ -7,7 +7,7 @@ const CardLinks = () => (
   <Card title="Links">
     <a href="https://vulcano.io/" target="_blank">Website</a><br />
     <a href="https://bitcointalk.org/index.php?topic=4951353" target="_blank">BitcoinTalk</a><br />
-    <a href="https://bitcoingarden.org/forum/index.php?topic=18163.0" traget="_blank">BitcoinGarden</a><br />
+    <a href="https://bitcoingarden.org/forum/index.php?topic=18163.0" target="_blank">BitcoinGarden</a><br />
     <a href="https://github.com/VulcanoCrypto" target="_blank">Github</a><br />
     <a href="https://discord.gg/wEUSbvS" target="_blank">Discord</a><br />
     <a href="https://twitter.com/vulcanocoin" target="_blank">Twitter</a><br />
